e015cde8bbb32fa1047c8f35bb1823ad4787dcff
ChainaChaina <lucasborgesmoraes@gmail.com>
2019-07-02 16:11:44 -0300

    Mudanças quase finais
    Rss acrescentado em uma nova página de notícias.
    Tentativa de JS

---
+++ script.js
@@ -0,0 +1,9 @@
+var pic = document.getElementById("carta")
+    
+
+function troca(){   
+    var num = Math.random() * 10;
+    if (num > 1) {
+        pic.src = "https://i.gifer.com/QGZW.gif"
+    }
+}
