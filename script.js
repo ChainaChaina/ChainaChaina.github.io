69e0f1339f28b887f3dd606b88bdf5857d90bea1
ChainaChaina <lucasborgesmoraes@gmail.com>
2019-07-03 07:28:13 -0300

    Add files via upload
    js funcionou

--- script.js
+++ script.js
@@ -1,9 +1,28 @@
-var pic = document.getElementById("carta")
-    
-
-function troca(){   
+function troca(){
+    var pic = document.getElementById("carta");
+    var text = document.getElementById("cartatexto");
     var num = Math.random() * 10;
-    if (num > 1) {
-        pic.src = "https://i.gifer.com/QGZW.gif"
+    console.log(num);
+    if (num < 3) {
+        pic.src="https://i.gifer.com/R4yV.gif";
+        text.innerHTML = "A concorrência olhando o site novo."
+    }
+    else if ( 3 < num && num < 5 ){
+        pic.src="https://i.gifer.com/5mZn.gif";
+        text.innerHTML = "Nossa equipe trabalhando."
+    }
+    else if ( 5 < num && num < 6 ){
+        pic.src="https://i.gifer.com/QmVN.gif";
+        text.innerHTML = "Estágiario no primeiro dia."
+    }
+    else if ( 6 < num && num < 7 ){
+        pic.src="https://i.gifer.com/JDhA.gif";
+        text.innerHTML = "As outras empresas..."
+    }
+    else if ( 7 < num && num < 10 ){
+        pic.src="https://i.gifer.com/6Xeg.gif";
+        text.innerHTML = "Estamos trabalhando duro pra você."
     }
 }
+
+
